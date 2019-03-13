9198159615abcd0b6683bad8cd5fee9a97e4ef62
qhg <110403962@qq.com>
2019-03-13 11:16:26 +0800

    添加文件

---
+++ WordInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ccy.Chat.Model
+{
+    public class WordInfo
+    {
+        public string Word { get; set; }
+
+        public int Len { get; set; }
+    }
+}
